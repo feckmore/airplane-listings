90c5e2d8e4dcaf6ee014ba46a3b8cc7e800005d6
David Schultz <dschultz@arthrex.com>
2022-01-16 20:27:14 -0600

    styling

--- src/card/card.tsx
+++ src/card/card.tsx
@@ -12,7 +12,7 @@ export const Card = ({listing}: CardProps) =>{
       <img src={listing.imagesource} alt={listing.title} />
       <h2>{listing.title}</h2>
       <p>{listing.description}</p>
-      <h3>{listing.price}</h3>
+      <h3 className='price'>{listing.price}</h3>
       <h3>{listing.regnum}</h3>
     </div>
     </a>
